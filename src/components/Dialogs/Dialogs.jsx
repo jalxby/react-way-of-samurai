fe7ce1085ad0899fe1a2638803db3f12dfd5d88b
Aleksander Jaroshevich <jalxby@gmail.com>
2022-07-24 17:35:22 +0300

    React 25

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -17,7 +17,7 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
-    let dialogsData = [
+    let dialogs= [
         {id: 1, name: 'Alexander'},
         {id: 2, name: 'Valera'},
         {id: 3, name: 'Ann'},
@@ -26,7 +26,7 @@ const Dialogs = (props) => {
         {id: 6, name: 'Witcher'}
     ]
 
-    let messagesData = [
+    let messages = [
         {id: 1, message: 'Hi!'},
         {id: 2, message: 'How are you?'},
         {id: 3, message: 'Yo!'},
@@ -35,26 +35,37 @@ const Dialogs = (props) => {
         {id: 6, message: 'And one more'}
     ]
 
+    let dialogElements = dialogs
+        .map(d => <Dialog name={d.name} num={d.id}/>);
+
+    let messagesElements = messages
+        .map(m => <Message message={m.message}/>);
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogs_items}>
-                <Dialog name={dialogsData[0].name} num={dialogsData[0].id}/>
-                <Dialog name={dialogsData[1].name} num={dialogsData[1].id}/>
-                <Dialog name={dialogsData[2].name} num={dialogsData[2].id}/>
-                <Dialog name={dialogsData[3].name} num={dialogsData[3].id}/>
-                <Dialog name={dialogsData[4].name} num={dialogsData[4].id}/>
-                <Dialog name={dialogsData[5].name} num={dialogsData[5].id}/>
 
+                {
+                    dialogElements
+                }
+                {/*<Dialog name={dialogsData[0].name} num={dialogsData[0].id}/>*/}
+                {/*<Dialog name={dialogsData[1].name} num={dialogsData[1].id}/>*/}
+                {/*<Dialog name={dialogsData[2].name} num={dialogsData[2].id}/>*/}
+                {/*<Dialog name={dialogsData[3].name} num={dialogsData[3].id}/>*/}
+                {/*<Dialog name={dialogsData[4].name} num={dialogsData[4].id}/>*/}
+                {/*<Dialog name={dialogsData[5].name} num={dialogsData[5].id}/>*/}
             </div>
 
             <div className={s.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
-                <Message message={messagesData[2].message}/>
-                <Message message={messagesData[3].message}/>
-                <Message message={messagesData[4].message}/>
-                <Message message={messagesData[5].message}/>
-
+                {
+                    messagesElements
+                }
+                {/*<Message message={messagesData[0].message}/>*/}
+                {/*<Message message={messagesData[1].message}/>*/}
+                {/*<Message message={messagesData[2].message}/>*/}
+                {/*<Message message={messagesData[3].message}/>*/}
+                {/*<Message message={messagesData[4].message}/>*/}
+                {/*<Message message={messagesData[5].message}/>*/}
             </div>
         </div>
     )
